d7339aaaaad6959edaaaa8df4f42ba72c51decc8
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-06-24 11:34:51 -0500

    added edit and delete functionality

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -1,16 +1,20 @@
 package com.codeup.blog.controllers;
 
+import com.codeup.blog.PostsDao.PostsRepository;
 import com.codeup.blog.models.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @Controller
 public class PostController {
+    private PostsRepository postsDao;
+    public PostController(PostsRepository postsRepository) {
+        postsDao = postsRepository;
+    }
+
     @GetMapping("/posts")
     public String postsIndex(Model model){
         ArrayList<Post> posts = new ArrayList<>();
@@ -28,7 +32,6 @@ public class PostController {
         return "posts/show";
     }
 
-
     @GetMapping("posts/create")
     @ResponseBody
     public String createPostForm(){
@@ -41,4 +44,29 @@ public class PostController {
         return "create a new post";
     }
 
+    @GetMapping("/posts/{id}/edit")
+    public String showEditForm(Model model, @PathVariable Long id) {
+        Post postToEdit = postsDao.getOne(id);
+        model.addAttribute("post", postToEdit);
+        return "posts/edit";
+    }
+
+    @PostMapping("/posts/{id}/edit")
+    @ResponseBody
+    public String editPost(@PathVariable long id,
+                           @RequestParam (name = "title") String title,
+                           @RequestParam (name = "body") String body) {
+        Post postToEdit = postsDao.getOne(id);
+        postToEdit.setTitle(title);
+        postToEdit.setBody(body);
+        postsDao.save(postToEdit);
+        return "Post Updated!";
+    }
+
+    @PostMapping("/posts/{id}/delete")
+    @ResponseBody
+    public String delete(@PathVariable long id){
+        postsDao.deleteById(id);
+        return "Post Deleted!";
+    }
 }
